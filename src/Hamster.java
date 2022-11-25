80977aeef77b2fe1296d2de4d7bcf18a7bfa9bdf
IEdelmann <iede@protonmail.com>
2022-11-25 08:43:00 +0100

    Hamster class: added methods for changing the direction and symbol

--- src/Hamster.java
+++ src/Hamster.java
@@ -1,4 +1,4 @@
-public class Hamster {
+public class Hamster extends MapField {
     private int direction;
     private int cornEaten;
     private Coordinates currentCoordinates;
@@ -6,6 +6,51 @@ public class Hamster {
     public Hamster() {
         this.direction = 0;
         this.cornEaten = 0;
+        this.fieldSymbol = '^';
+    }
+
+    public void changeDirection(int directionInstruction) {
+        if (directionInstruction >= -1 && directionInstruction <= 1) {
+            directionConverter(directionInstruction);
+            changeDirectionOfSymbol();
+        }
+    }
+
+    private void directionConverter(int directionChange) {
+        int temp = this.getDirection() + directionChange;
+
+        if (temp > 3) {
+            this.setDirection(0);
+        }
+
+        if (temp < 0) {
+            this.setDirection(3);
+        }
+    }
+
+    private void changeDirectionOfSymbol() {
+        switch (this.getDirection()) {
+
+            case 0 -> {
+                this.setFieldSymbol('^');
+            }
+
+            case 1 -> {
+                this.setFieldSymbol('>');
+            }
+
+            case 2 -> {
+                this.setFieldSymbol('v');
+            }
+
+            case 3 -> {
+                this.setFieldSymbol('<');
+            }
+
+            default -> {
+                System.out.println("If you see this line, you somehow broke the code!");
+            }
+        }
     }
 
     public boolean moveForward(MapField mapField) {
