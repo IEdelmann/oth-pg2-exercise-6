9bf3671f66ac54be33e553aa71c5bcdcc305dbe9
IEdelmann <iede@protonmail.com>
2022-11-28 19:32:29 +0100

    Main class: edited the intro syso for legal reasons

--- src/Main.java
+++ src/Main.java
@@ -21,13 +21,13 @@ public class Main {
             gameMapDynamicChances.printGameMap();
             System.out.println("Corns eaten: " + hamster.getCornEaten());
 
-            directionInput = userInput.userInputString("Direction: ", "[w, a, s, d, x]", 1).charAt(0);
+            directionInput = userInput.userInputString("Direction: ", "[w, a, s, d, y]", 1).charAt(0);
 
             // Prints 50 new lines to display the game better;
 //            System.out.println(new String(new char[50]).replace("\0", "\r\n"));
 
-            if (directionInput == 'x') {
-                System.out.println("Thank you for playing the official xHamster game :D");
+            if (directionInput == 'y') {
+                System.out.println("Thank you for playing the official yHamster game :D\n");
                 runTimeFlag = false;
             }
 
@@ -36,9 +36,10 @@ public class Main {
     }
 
     public static void printIntro() {
-        System.out.println("\nWelcome to the xHamster game!");
+        System.out.println("\nWelcome to the yHamster game!");
         System.out.println("You can maneuver by 'w', 'a', 's' and 'd'.");
         System.out.println("Eat as many corns '*' as possible.");
-        System.out.println("To exit the game enter 'x' - that is why the game's called xHamster... in case you were wondering...\n");
+        System.out.println("To exit the game enter 'y' - that is why the game's called yHamster.");
+        System.out.println("To avoid legal issues with a large online platform of adult videos, I have decided to call now the game 'yHamster' instead of the original name.\n");
     }
 }
